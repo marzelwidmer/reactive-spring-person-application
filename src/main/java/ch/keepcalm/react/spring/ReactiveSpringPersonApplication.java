59560c025e627f93fcaa06da7bd8639ef7178c6b
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-28 07:20:10 +0200

    first commit

---
+++ src/main/java/ch/keepcalm/react/spring/ReactiveSpringPersonApplication.java
@@ -0,0 +1,12 @@
+package ch.keepcalm.react.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSpringPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSpringPersonApplication.class, args);
+	}
+}
