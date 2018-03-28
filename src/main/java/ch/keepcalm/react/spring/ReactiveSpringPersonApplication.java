b0e356d9f9673ac7974f145fb7a1dcee18fb7cbe
Marcel Widmer <marzelwidmer@gmail.com>
2018-03-28 07:21:54 +0200

    add reactive spring anotations

--- src/main/java/ch/keepcalm/react/spring/ReactiveSpringPersonApplication.java
+++ src/main/java/ch/keepcalm/react/spring/ReactiveSpringPersonApplication.java
@@ -2,8 +2,12 @@ package ch.keepcalm.react.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableWebFluxSecurity
 public class ReactiveSpringPersonApplication {
 
 	public static void main(String[] args) {
